bcc0bd7409b49008581f4fbb07c47374bf589b9e
johnpaulduffy <157059406+johnpaulduffy@users.noreply.github.com>
2024-04-23 18:39:03 +0000

    basic game loop

---
+++ pong.py
@@ -0,0 +1,29 @@
+
+import pygame
+
+pygame.init()
+running = true
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+clock = pygame.time.Clock()
+
+
+
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False    
+
+       
+pygame.quit()
+
+
+
+
+
+
+
